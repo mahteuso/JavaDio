e88607c0c54b883698f57da54fc1614b6487437c
Mateus <mateus@ufscar.br>
2024-03-15 12:06:35 -0300

    Config: Atualizando CurrentAccount

--- desafio-dio-banco/src/CurrentAccount.java
+++ desafio-dio-banco/src/CurrentAccount.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class CurrentAccount extends Account{
     private double balance;
-    TypeAccount typeAccount;
+    
 
     public CurrentAccount(Client client, TypeAccount typeAccount) {
         super(client, typeAccount);
