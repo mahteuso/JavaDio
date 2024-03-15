ad2d4806f994c5c0645d3461032c4a19731b9732
Mateus <mateus@ufscar.br>
2024-03-15 12:10:18 -0300

    Config: Criando IAccount

--- desafio-dio-banco/src/IAccount.java
+++ desafio-dio-banco/src/IAccount.java
@@ -1,6 +1,7 @@
 public interface IAccount {
+    
     public abstract void draw(double value);
     public abstract void cash(double value);
     public abstract void transfer(Account accountDestiny, double value);
-    
+
 }
