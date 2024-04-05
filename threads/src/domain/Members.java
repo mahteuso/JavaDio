ee8f6d46f58fd53b8fb6a23dec63a6759d48de72
Mateus <mateus@ufscar.br>
2024-04-05 09:12:13 -0300

    Estudo sobre sincronismo de Threads

--- threads/src/domain/Members.java
+++ threads/src/domain/Members.java
@@ -1,5 +1,53 @@
 package domain;
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Members {
-    
+    private final Queue<String> emails = new ArrayBlockingQueue<>(10);
+    private boolean open = true;
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public int pedingEmails() {
+        synchronized (emails) {
+            return emails.size();
+        }
+
+    }
+
+    public void addMemberEmail(String email) {
+        synchronized (emails) {
+            System.out.println(Thread.currentThread().getName() + " Adicionando email na lista");
+            emails.add(email);
+            emails.notifyAll();
+        }
+    }
+
+    public void retrieveEmail() {
+        System.out.println(Thread.currentThread().getName() + " Checando se term emails");
+        synchronized (emails) {
+            while (open) {
+                if (emails.isEmpty()) break;
+                System.out.println(Thread.currentThread().getName() + " Esperando liberação de email");
+                try {
+                    System.out.println(Thread.currentThread().getName() + " Esperando email, entrando em modo de espera");
+                    emails.wait();
+                    String email = emails.poll();
+                    System.out.println(Thread.currentThread().getName() + " Acabou de recolher o email" + email);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public void close() {
+        open = false;
+        synchronized (emails) {
+            System.out.println(Thread.currentThread().getName() + " Notificando a todos que não estamos mais pegando emails");
+        }
+    }
 }
